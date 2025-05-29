4e4601b6d3c2e5c17f7941d9f27e5d7055656591
Nuzera <nachonuzera@gmail.com>
2025-05-28 23:26:13 -0300

    Add files via upload

---
+++ TP-Listas/TP LISTAS/act-3.py
@@ -0,0 +1,13 @@
+#creamos una lista vacia
+
+lista_vacia = []
+
+#agregamos tres elementos con append
+
+lista_vacia.append ("python")
+lista_vacia.append ("programacion")
+lista_vacia.append ("actividad") 
+
+#imprimimos la lista con los elementos agregados
+
+print("lista final:", lista_vacia)
