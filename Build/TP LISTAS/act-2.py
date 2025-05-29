386d2be88bc9fecfe99c0d23865fe882f7758b1a
Nuzera <nachonuzera@gmail.com>
2025-05-28 23:24:51 -0300

    Add files via upload

---
+++ Build/TP LISTAS/act-2.py
@@ -0,0 +1,11 @@
+#creamos una lista con 5 elementos 
+
+lista =["A", "B", "C", "D", "E"]
+
+#utilizamos la indexacion negativa para mostrar el penultimo elemento
+
+penultimo_elemento = lista [-2]
+
+#imprimimos el elemento seleccionado 
+
+print("el penultimo elemento es: ", penultimo_elemento) 
