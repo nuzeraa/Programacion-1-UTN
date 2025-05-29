591803bdbd85379ab42329c8f665ceb27ccd1307
Nuzera <nachonuzera@gmail.com>
2025-05-28 23:25:10 -0300

    Delete Build directory

--- Build/TP LISTAS/act-1.py
+++
@@ -1,8 +0,0 @@
-#Creamos una lista con los numeros del 1 al 100 que sean múltiplos de 4
-#Usamos range desde 4 hasta 101 (no se incluye) con paso 4
-
-multiplos_4 = list(range(4,101,4))
-
-#imprimimos la lista generada
-
-print(multiplos_4)
--- Build/TP LISTAS/act-6.py
+++
@@ -1,7 +0,0 @@
-#creamos una lista con numeros del 10 al 30 (incluido) con paso 5
-
-numeros = list(range(10,31,5))
-
-#imprimimos solo los dos primeros numeros 
-
-print (numeros[:2])
