4920a57e3d6409ecdd38f323cfd8e92940e5a4e9
Nuzera <nachonuzera@gmail.com>
2025-05-28 23:28:36 -0300

    Delete TP-Listas/TP LISTAS directory

--- TP-Listas/TP LISTAS/act-10.py
+++
@@ -1,7 +0,0 @@
-#creamos la lista anidada con los elementos correspondientes
-
-lista_anidada = [15, True, [25.5, 57.9, 30.6], False]
-
-#imprimimos la lista
-
-print("lista anidada:", lista_anidada)
